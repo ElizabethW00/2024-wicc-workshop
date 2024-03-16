67b81b5f0cf1cb8ceefb19c1f6c8d25076263c37
ElizabethW00 <ellewoo12@gmail.com>
2024-03-15 23:29:28 -0400

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import email from "/src/img/email.svg";
 import phone from "/src/img/phone.svg";
+import linkedinlogo from "/src/img/linkedin.svg";
 const Footer = () => {
   return (
     <footer className="bg-[#ffdee3] rounded-tl-3xl rounded-tr-3xl p-10 flex flex-col space-y-4 px-24 test-base">
@@ -13,10 +14,10 @@ const Footer = () => {
           <p>erw96@cornell.edu</p>
         </div>
 
-        {/* <div className="flex flex-row items-center space-x-3">
-          <img src={phone} />
-          <p>123-456-7890</p>
-        </div> */}
+        <div className="flex flex-row items-center space-x-3">
+          <img src={linkedinlogo} />
+          <p>www.linkedin.com/in/elizabethwoo5</p>
+        </div>
       </div>
       <div>
         <p className="flex justify-center text-sm">
